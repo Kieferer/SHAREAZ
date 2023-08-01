4e8d43c3c47751c236e1a46552f19faad40bac20
Kieferer <balazsborsi0@gmail.com>
2023-08-01 22:20:37 +0200

    feat: Create FileRecevier in Services folder

---
+++ Services/FileReceiver.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Maui.Controls.PlatformConfiguration;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHAREAZ.Services
+{
+    public static class FileReceiver
+    {
+        public static string PERCENTAGE = String.Empty;
+        private readonly static int PORT = 9999;
+        public static async void Receive(string saveDirectoryPath)
+        {
+            IPAddress ipAddress = IPAddress.Any;
+            TcpListener listener = new(ipAddress, PORT);
+            listener.Start();
+
+            TcpClient client = await listener.AcceptTcpClientAsync();
+            NetworkStream networkStream = client.GetStream();
+
+            BinaryReader binaryReader = new BinaryReader(networkStream);
+            string fileName = binaryReader.ReadString();
+            long fileSize = binaryReader.ReadInt64();
+
+            string savePath = Path.Combine(saveDirectoryPath, fileName);
+            string directoryPath = Path.GetDirectoryName(savePath);
+            Directory.CreateDirectory(directoryPath);
+
+            FileStream fileStream = new FileStream(savePath, FileMode.Create, FileAccess.Write);
+            byte[] buffer = new byte[8192];
+            int bytesRead;
+
+            long totalBytesRead = 0;
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Debug.WriteLine(fileSize * 100 == fileStream.Length);
+            while ((bytesRead = await networkStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+            {
+                await fileStream.WriteAsync(buffer, 0, bytesRead);
+
+                totalBytesRead += bytesRead;
+                double progressPercentage = (double)totalBytesRead / fileSize * 100;
+                PERCENTAGE = $"{progressPercentage:F2}%";
+            }
+
+            stopwatch.Stop();
+            networkStream.Close();
+        }
+    }
+}
